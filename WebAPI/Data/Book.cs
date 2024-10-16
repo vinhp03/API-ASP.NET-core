8408f8cdae92561298364ea1e7f5f501556742b8
vinhp03 <thevinhp333@gmail.com>
2024-10-16 22:09:12 +0700

    Creata API .Net core

---
+++ WebAPI/Data/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI.Data
+{
+    [Table("Book")]
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        [MaxLength(100)]
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        [Range (0, double.MaxValue)]
+        public double Price { get; set; }
+
+    }
+}
---
+++ WebAPI/Data/BookStoreContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebAPI.Data
+{
+    public class BookStoreContext : DbContext
+    {
+        public BookStoreContext(DbContextOptions<BookStoreContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set;}
+
+    }
+}
